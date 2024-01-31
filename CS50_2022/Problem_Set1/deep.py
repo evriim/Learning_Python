d6797239e12fed849411c77a264d0190b817aa48
Evrim Ustabaş <71992453+evriim@users.noreply.github.com>
2024-01-31 16:08:57 +0300

    Add files via upload
    https://cs50.harvard.edu/python/2022/psets/1/deep/

---
+++ CS50_2022/Problem_Set1/deep.py
@@ -0,0 +1,8 @@
+#Get input from user
+answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+
+#Create condition for correctness of answer
+if answer == "forty-two" or answer == "forty two" or answer == "42":
+    print("Yes")
+else:
+    print("No")
