4b0b8ea032b9143795600c622c8ba8e9927be7d3
Evrim Ustabaş <71992453+evriim@users.noreply.github.com>
2024-03-05 14:44:51 +0300

    camel.py

--- CS50_2022/Problem_Set2/camel.py
+++ CS50_2022/Problem_Set2/camel.py
@@ -1,4 +1,7 @@
+#Retrieving data from the user.
 camelCase = input("camelCase: ").strip
+
+#Returning data from camelCase to snake form
 for i in camelCase:
     if i == i.upper():
         camelCase=camelCase.replace(i,"_"+i)
