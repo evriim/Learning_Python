2d673578461835ceffaafa045a745363563aed9b
Evrim Ustabaş <71992453+evriim@users.noreply.github.com>
2024-03-05 14:18:32 +0300

    Add files via upload
    “All right,” said the computer, and settled into silence again. The two men fidgeted. The tension was unbearable.
    “You’re really not going to like it,” observed Deep Thought.
    “Tell us!”
    “All right,” said Deep Thought. “The Answer to the Great Question…”
    “Yes…!”
    “Of Life, the Universe and Everything…” said Deep Thought.
    “Yes…!”
    “Is…” said Deep Thought, and paused.
    “Yes…!”
    “Is…”
    “Yes…!!!…?”
    “Forty-two,” said Deep Thought, with infinite majesty and calm.”
    
    — The Hitchhiker’s Guide to the Galaxy, Douglas Adams
    
    In deep.py, implement a program that prompts the user for the answer to the Great Question of Life, the Universe and Everything, outputting Yes if the user inputs 42 or (case-insensitively) forty-two or forty two. Otherwise output No.
    
    https://cs50.harvard.edu/python/2022/psets/1/deep/

---
+++ CS50_2022/Problem_Set1/deep.py
@@ -0,0 +1,8 @@
+#Retrieving data from the user.
+answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+
+#Checking for accuracy on the requested state.
+if answer == "forty-two" or answer == "forty two" or answer == "42":
+    print("Yes")
+else:
+    print("No")
